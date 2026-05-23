ef3d7e676295630e83fbf40785b5e0ef00729fea
mayankinkk <mayank.170805@gmail.com>
2026-05-23 16:28:13 +0530

    fix(ThemeProvider): properly render children to resolve blank page issue

--- miiam-app/src/components/ThemeProvider.tsx
+++ miiam-app/src/components/ThemeProvider.tsx
@@ -3,13 +3,13 @@
 import { useEffect } from "react";
 import { useThemeStore } from "@/lib/store/themeStore";
 
-export default function ThemeProvider() {
+export default function ThemeProvider({ children }: { children: React.ReactNode }) {
   const theme = useThemeStore((s) => s.theme);
   const setTheme = useThemeStore((s) => s.setTheme);
 
   useEffect(() => {
     setTheme(theme);
-  }, []);
+  }, [theme, setTheme]);
 
-  return null;
+  return <>{children}</>;
 }
