d9e1c05d89080acdff8329319ce0ead34af4aed1
mayankinkk <mayank.170805@gmail.com>
2026-06-11 02:56:15 +0530

    fix: correct import order in checkout-utils.ts

--- miiam-app/src/lib/checkout-utils.ts
+++ miiam-app/src/lib/checkout-utils.ts
@@ -1,5 +1,6 @@
-const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 import { PRINT_MENU_ITEM_ID } from "@/lib/constants";
+
+const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
 export function safeMenuItemId(id: string) {
   return UUID_RE.test(id) ? id : PRINT_MENU_ITEM_ID;
