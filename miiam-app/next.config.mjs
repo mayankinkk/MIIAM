eff87059698d2d0f077a49453674dac0f1a93f13
mayankinkk <mayank.170805@gmail.com>
2026-06-22 12:38:21 +0530

    fix: remove deprecated disableLogger from Sentry config

--- miiam-app/next.config.mjs
+++ miiam-app/next.config.mjs
@@ -109,6 +109,5 @@ export default withSentryConfig(nextConfig, {
   silent: true,
   widenClientFileUpload: true,
   hideSourceMaps: true,
-  disableLogger: true,
   tunnelRoute: "/api/sentry",
 });
